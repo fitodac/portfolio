45d7e55a301728e0e4c8abf6ecb55bec84257c45
fitodac <fitodac@gmail.com>
2023-08-16 14:27:33 -0300

    js/scripts.js

---
+++ synergy/js/scripts.js
@@ -0,0 +1,31 @@
+﻿
+
+/*------------------------------------------------------------------------------*/
+/* SCRIPTS.js */
+/* Here you can write all javascript. */
+/*------------------------------------------------------------------------------*/
+
+
+var loop = false;
+
+$(document).ready(function(){
+	
+	arm();
+
+
+});//document ready end
+
+
+function arm(){
+	setTimeout(function(){
+		$('.androide_arm').addClass('anim');
+		
+		setTimeout(function(){
+			$('.androide_arm').removeClass('anim');
+		}, 5000);
+		
+		if(loop == false){
+			arm();
+		}
+	}, 6000);
+}
